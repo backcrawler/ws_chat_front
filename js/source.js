2021dc1e8208c80258e6e78ce40ce9c95b6df714
backcrawler <mymail@xxx.com>
2021-09-17 22:39:49 +0300

    Polling, extras

--- js/source.js
+++ js/source.js
@@ -1,41 +1,31 @@
 let ws;
 let wsClosed = true;
+deltaPoll();
 
-fetch("http://localhost:8000/init").then(
-    r => {
-        console.log(r, document.cookie);
-        fetch('http://localhost:8000/test').then(
-            r => console.log(r, document.cookie)
-        )
-    }
-)
-
-
-
-async function sendMessage(event) {
-    if (!ws || wsClosed) {
-        await initConnection();
-        const testReq = await fetch('http://localhost:8000/test');
-        console.log(await testReq.json())
-        createWS();
-    }
-
-    const input = document.getElementById("messageText")
-    ws.send(JSON.stringify({val: input.value}))
-    input.value = ''
-    event.preventDefault()
+function sendMessage(event) {
+    initConnection();
+    // if (!ws || wsClosed) {
+    //     console.log('here')
+    //     await initConnection();
+    //     createWS();
+    // }
+    // console.log('here2')
+    // const input = document.getElementById("messageText")
+    // ws.send(JSON.stringify({val: input.value}))
+    // input.value = ''
+    event.preventDefault();
 }
 
 
 function createWS() {
-    ws = new WebSocket("ws://localhost:8000/ws");
+    ws = new WebSocket("ws://127.0.0.1:8000/ws");
     wsClosed = false;
     ws.onmessage = function(event) {
-        const messages = document.getElementById('messages')
-        const message = document.createElement('li')
-        const content = document.createTextNode(event.data)
-        message.appendChild(content)
-        messages.appendChild(message)
+        const messages = document.getElementById('messages');
+        const message = document.createElement('li');
+        const content = document.createTextNode(event.data);
+        message.appendChild(content);
+        messages.appendChild(message);
     }
     ws.onclose = function (event) {
         wsClosed = true;
@@ -44,5 +34,60 @@ function createWS() {
 
 
 async function initConnection() {
-    await fetch("http://localhost:8000/init");
+    const r = await fetch("http://127.0.0.1:8000/init");
+    console.log(await r.json());
+    console.log(document.cookie);
+    const r2 = await fetch("http://127.0.0.1:8000/test");
+    console.log(await r2.json());
+
+}
+
+
+function deltaPoll(chId) {
+    let pollInterval = 0;
+    fetch(`http://127.0.0.1:8000/delta?ch_id=${chId}`)
+        .then((r) => {
+            if (r.status !== 200) {
+                console.log('GOT WRONG STATUS:', r.status);
+                pollInterval = 1000;
+            } else {
+                r.json()
+                    .then((json) => {
+                        console.log('json:', json)
+                        if (json.result && json.result !== 'reinit-required') {
+                            processDeltas(json.result);
+                        }
+                    })
+                pollInterval = 0;
+            }
+        })
+        .catch((err) => {
+            console.log('ERR:', err);
+            pollInterval = 1000;
+        })
+        .finally(() => {
+            console.log('REINIT');
+            setTimeout(() => deltaPoll(chId), pollInterval)
+        })
+}
+
+
+function processDeltas(deltas) {
+
+}
+
+
+async function fetchWithTimeout(resource, options = {}) {
+  const { timeout = 8000 } = options;
+
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal
+  });
+  clearTimeout(id);
+
+  return response;
 }
